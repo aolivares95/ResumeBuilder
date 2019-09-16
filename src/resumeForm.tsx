cde7e9437d906f2f96d009b4dfb36570ade38158
Antonio Olivares <antonio.olivares1995@gmail.com>
2019-09-15 21:09:32 -0400

    added new button and added interface to please typescript

--- src/resumeForm.tsx
+++ src/resumeForm.tsx
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
+import { string, array } from 'prop-types';
 
 let style ={
     padding:"10px"
 }
 
-class Resume extends Component{
+//add interface
+interface IResume{
+    name:string,
+    phoneNumber:string,
+    education:string[],    
+} 
+
+
+class Resume extends Component<any,IResume>{
+    
+    constructor(props:any){
+        super(props);
+            this.state={name:"",
+                        phoneNumber:"",
+                        education:[],
+                        };
+           
+    this.handleNameChange=this.handleNameChange.bind(this)
+    this.handleNumberChange=this.handleNumberChange.bind(this)
+    this.handleEducationChange=this.handleEducationChange.bind(this)
+    }
+
+    handleNameChange(event:any){
+        this.setState({name:event.target.value});
+    }
+    handleNumberChange(event:any){
+        this.setState({phoneNumber:event.target.value});
+    }
+    handleEducationChange(event:any){
+        this.setState({education:event.target.value});
+    }
     render(){
         return(
 
-            <div>
+            <form>
                 <label style={style}>Please enter your name</label>
-                <input/>
-                </div>
+                <input type="text" value={this.state.name} onChange={this.handleNameChange}/>
+                <label style={style}>Please enter your phone number</label>
+                <input type="text" value={this.state.phoneNumber} onChange={this.handleNumberChange}/>
+                <label style={style}>Please enter your education history</label>
+                <input type="text" value={this.state.education} onChange={this.handleEducationChange}/>
+                <button>Add to history</button>
+                <div><label>Your education: {this.state.education}</label></div>
+                </form>
         )      
     };
 };
+
 
 export default Resume
